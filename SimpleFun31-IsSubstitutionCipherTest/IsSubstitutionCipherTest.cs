55993d88109298416a6f837c56b868a8ef7851b7
Angus <ajdfajdfl2003@gmail.com>
2017-03-18 11:59:41 +0800

    Finish SimpleFun31 IsSubstitutionCipher

---
+++ SimpleFun31-IsSubstitutionCipherTest/IsSubstitutionCipherTest.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleFun31_IsSubstitutionCipherTest
+{
+    [TestClass]
+    public class IsSubstitutionCipherTest
+    {
+        [TestMethod]
+        public void string1_And_string2_Has_Same_Length()
+        {
+            //arrange
+            string string1 = "abc";
+            string string2 = "aaa";
+            var target = new SimpleSubstitutionCipher();
+
+            //act
+            bool actual = target.HasSameLength(string1, string2);
+
+            //assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void StringLength_is_LargerEqual_than_1_and_LessEqual_than_10()
+        {
+            string string1 = "a";
+            var target = new SimpleSubstitutionCipher();
+
+            //act
+            bool actual = target.CheckLength(string1);
+
+            //assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void StringLength_isnot_LargerEqual_than_1_and_LessEqual_than_10()
+        {
+            string string1 = "aaaaaaaaaaa";
+            var target = new SimpleSubstitutionCipher();
+
+            //act
+            var actual = target.CheckLength(string1);
+
+            //assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void aacb_And_aabc_Is_SubstitutionCipher()
+        {
+            //arrange
+            var target = new SimpleSubstitutionCipher();
+            var string1 = "aacb";
+            var string2 = "aabc";
+
+            //act
+            var actual = target.IsSubstitutionCipher(string1, string2);
+
+            //assert
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod]
+        public void aa_And_bc_Is_Not_SubstitutionCipher()
+        {
+            //arrange
+            var target = new SimpleSubstitutionCipher();
+            var str1 = "aa";
+            var str2 = "bc";
+
+            //act
+            var actual = target.IsSubstitutionCipher(str1, str2);
+
+            //assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void aaxxaaz_And_aazzaay_IsSubstitutionCipher()
+        {
+            //arrange
+            var target = new SimpleSubstitutionCipher();
+            var str1 = "aaxxaaz";
+            var str2 = "aazzaay";
+
+            //act
+            var actual = target.IsSubstitutionCipher(str1, str2);
+
+            //assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void aaxyaa_And_aazzaa_IsNotSubstitutionCipher()
+        {
+            //arrange
+            var target = new SimpleSubstitutionCipher();
+            var str1 = "aaxyaa";
+            var str2 = "aazzaa";
+
+            //act
+            var actual = target.IsSubstitutionCipher(str1, str2);
+
+            //assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod]
+        public void aabc_And_aacb_IsSubstitutinoCipher()
+        {
+            var target = new SimpleSubstitutionCipher();
+            var str1 = "aabc";
+            var str2 = "aacb";
+
+            var actual = target.IsSubstitutionCipher(str1, str2);
+
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod]
+        public void dccd_And_zzxx_IsNotSubstitutionCipher()
+        {
+            var  target = new SimpleSubstitutionCipher();
+            var str1 = "dccd";
+            var str2 = "zzxx";
+
+            var actual = target.IsSubstitutionCipher(str1, str2);
+            
+            Assert.IsFalse(actual);
+        }
+    }
+}
---
+++ SimpleFun31-IsSubstitutionCipherTest/SimpleSubstitutionCipher.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleFun31_IsSubstitutionCipherTest
+{
+    public class SimpleSubstitutionCipher
+    {
+        private Dictionary<string, string> _cipherText;
+
+        public bool IsSubstitutionCipher(string string1, string string2)
+        {
+            _cipherText = new Dictionary<string, string>();
+
+            if (!CheckLength(string1))
+            {
+                return false;
+            }
+            if (!HasSameLength(string1, string2))
+            {
+                return false;
+            }
+            for (var i = 0; i < string1.Length; i++)
+            {
+                var str1 = string1.Substring(i, 1);
+                var str2 = string2.Substring(i, 1);
+                if (!_cipherText.ContainsKey(str1) && !_cipherText.ContainsValue(str2))
+                {
+                    _cipherText.Add(str1, str2);
+                }
+                if (!_cipherText.ContainsKey(str1))
+                {
+                    return false;
+                }
+                else if (!_cipherText[str1].Equals(str2))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool HasSameLength(string string1, string string2)
+        {
+            return string1.Length.Equals(string2.Length);
+        }
+
+        public bool CheckLength(string string1)
+        {
+            return string1.Length <= 10 && string1.Length >= 1;
+        }
+    }
+}
