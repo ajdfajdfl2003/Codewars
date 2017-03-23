b2692e356e8ec9cdb22699ef5ba622aec795e7d8
Angus <ajdfajdfl2003@gmail.com>
2017-03-24 07:18:46 +0800

    Finitsh Are they the "same"?
    Are they the same project init
    
    [Red] Input a cannot be null.
    
    [Green] Input a cannot be null.
    
    [Red] Input_b_cannot_be_NULL
    
    [Green] Input_b_cannot_be_NULL
    
    [Refactor]
    
    [Red] Input_a_And_b_ShouldNotbeTheSame
    
    [Green] Input_a_And_b_ShouldNotbeTheSame
    
    [Red] Input_a_And_b_ShouldbeTheSame
    
    [Green] Input_a_And_b_ShouldbeTheSame
    
    [Refactor]

---
+++ AreTheyTheSame/AreTheySame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace AreTheyTheSame
+{
+    public class AreTheySame
+    {
+        public bool comp(int[] a, int[] b)
+        {
+            if (a == null)
+            {
+                return false;
+            }
+            else if (b == null)
+            {
+                return false;
+            }
+            else
+            {
+                var newA = a.Select(element => (int)Math.Pow(element, 2)).ToArray();
+                Array.Sort(b);
+                Array.Sort(newA);
+                return b.SequenceEqual(newA);
+            }
+        }
+    }
+}
