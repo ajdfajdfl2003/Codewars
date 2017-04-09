a6812eb5d699906087b6bad661fb0d80fa927f38
Angus <ajdfajdfl2003@gmail.com>
2017-04-09 13:49:15 +0800

    Finish Invert values
    [Red] Add TestInvertValues_negative_1_to_negative_5 Test case
    
    [Green] Add TestInvertValues_negative_1_to_negative_5 Test case
    
    [Green] Add TestInvertValues_negative1_2_negative3_4_negative5 Test case
    
    [Green] Add TestInvertValues_Null_Array Test case
    
    [Green] Add TestInvertValues_0 Test case
    
    Refactor

---
+++ InvertValuesTest/ArraysInversion.cs
@@ -0,0 +1,18 @@
+﻿namespace InvertValuesTest
+{
+    public class ArraysInversion
+    {
+        public ArraysInversion()
+        {
+        }
+
+        public int[] InvertValues(int[] input)
+        {
+            for (var i = 0; i < input.Length; i++)
+            {
+                input[i] *= -1;
+            }
+            return input;
+        }
+    }
+}
---
+++ InvertValuesTest/InvertValuesTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InvertValuesTest
+{
+    [TestClass]
+    public class InvertValuesTest
+    {
+        [TestMethod]
+        public void TestInvertValues_0()
+        {
+            var target = new ArraysInversion();
+
+            var actual = target.InvertValues(new int[] { 0 });
+
+            var expected = new int[] { 0 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestInvertValues_Null_Array()
+        {
+            var target = new ArraysInversion();
+
+            var actual = target.InvertValues(new int[] { });
+
+            CollectionAssert.AreEqual(new int[] { }, actual);
+        }
+
+        [TestMethod]
+        public void TestInvertValues_negative1_2_negative3_4_negative5()
+        {
+            var target = new ArraysInversion();
+
+            var actual = target.InvertValues(new int[] { 1, -2, 3, -4, 5 });
+
+            var expected = new int[] { -1, 2, -3, 4, -5 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestInvertValues_negative_1_to_negative_5()
+        {
+            //arrange
+            var target = new ArraysInversion();
+
+            //act
+            var actual = target.InvertValues(new int[] { 1, 2, 3, 4, 5 });
+
+            //assert
+            var expected = new int[] { -1, -2, -3, -4, -5 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
