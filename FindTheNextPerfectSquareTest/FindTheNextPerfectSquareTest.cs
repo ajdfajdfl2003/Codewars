a4b2de0f423e8b6311aa8c4f48ac0099ca506924
Angus <ajdfajdfl2003@gmail.com>
2017-04-09 17:35:07 +0800

    Finish Find the next perfect square!
    Find the next perfect square! Test Init
    
    [Red] Add TestFindNextSquare_121_Result_144 Test case
    
    [Green] TestFindNextSquare_121_Result_144
    
    [Green] Add TestFindNextSquare_625_Result_676 Test case
    
    [Green] Add TestFindNextSquare_319225_Result_320356 Test case
    
    [Green] Add TestFindNextSquare_15241383936_Result_15241630849 Test case
    
    [Red] Add TestFindNextSquare_155_Result_negative_1 Test case
    
    [Green] TestFindNextSquare_155_Result_negative_1
    
    Refactor
    
    [Red] Add TestFindNextSquare_921227816_Result_negative_1 Test case
    
    [Green] TestFindNextSquare_921227816_Result_negative_1

---
+++ FindTheNextPerfectSquareTest/FindTheNextPerfectSquareTest.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FindTheNextPerfectSquareTest
+{
+    [TestClass]
+    public class FindTheNextPerfectSquareTest
+    {
+        [TestMethod]
+        public void TestFindNextSquare_921227816_Result_negative_1()
+        {
+            var target = new Kata();
+
+            var num = 921227816;
+            var actual = target.FindNextSquare(num);
+
+            var expected = -1;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestFindNextSquare_155_Result_negative_1()
+        {
+            var target = new Kata();
+
+            var num = 155;
+            var actual = target.FindNextSquare(num);
+
+            var expected = -1;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestFindNextSquare_15241383936_Result_15241630849()
+        {
+            var target = new Kata();
+
+            var num = 15241383936;
+            var actual = target.FindNextSquare(num);
+
+            var expected = 15241630849;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestFindNextSquare_319225_Result_320356()
+        {
+            var target = new Kata();
+
+            var num = 319225;
+            var actual = target.FindNextSquare(num);
+
+            var expected = 320356;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestFindNextSquare_625_Result_676()
+        {
+            var target = new Kata();
+
+            var num = 625;
+            var actual = target.FindNextSquare(num);
+
+            var expected = 676;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestFindNextSquare_121_Result_144()
+        {
+            var target = new Kata();
+
+            var num = 121;
+            var actual = target.FindNextSquare(num);
+
+            var expected = 144;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ FindTheNextPerfectSquareTest/Kata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FindTheNextPerfectSquareTest
+{
+    public class Kata
+    {
+        public long FindNextSquare(long num)
+        {
+            if (Math.Round(Math.Sqrt(num)) != Math.Sqrt(num))
+            {
+                return -1;
+            }
+            else
+            {
+                var nextRoot = Math.Sqrt(num) + 1;
+                return (long)Math.Pow(nextRoot, 2);
+            }
+        }
+    }
+}
