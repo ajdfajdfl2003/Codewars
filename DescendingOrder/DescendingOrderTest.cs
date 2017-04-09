e66462e952cdee08a4a0945dac7d5e6cb9db7dc5
Angus <ajdfajdfl2003@gmail.com>
2017-04-09 16:36:36 +0800

    Finish Descending Order
    DescendingOrder init
    
    [Red] Add TestDescendingOrder_Input_21445_Output_54421 Test case
    
    [Green] TestDescendingOrder_Input_21445_Output_54421
    
    [Green] Add TestDescendingOrder_Input_145263_Output_654321 Test case

---
+++ DescendingOrder/DescendingOrderTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DescendingOrder
+{
+    [TestClass]
+    public class DescendingOrderTest
+    {
+        [TestMethod]
+        public void TestDescendingOrder_Input_145263_Output_654321()
+        {
+            var target = new Kata();
+
+            var num = 145263;
+            var actual = target.DescendingOrder(num);
+
+            var expected = 654321;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestDescendingOrder_Input_21445_Output_54421()
+        {
+            var target = new Kata();
+
+            var num = 21445;
+            var actual = target.DescendingOrder(num);
+
+            var expected = 54421;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ DescendingOrder/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using static System.Int32;
+
+namespace DescendingOrder
+{
+    public class Kata
+    {
+        public Kata()
+        {
+        }
+
+        public int DescendingOrder(int num)
+        {
+            var result = Parse(num.ToString().ToCharArray().OrderByDescending(x => x).Select(x => x.ToString()).Aggregate((prev, next) => prev + next));
+            return result;
+        }
+    }
+}
