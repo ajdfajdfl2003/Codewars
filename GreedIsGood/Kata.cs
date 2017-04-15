c7233ef824484cd2df52cd6f53d9848e2dc92cdc
Angus <ajdfajdfl2003@gmail.com>
2017-04-15 08:52:13 +0800

    Finish Greed is Good
    Greed is Good ! Test init
    
    [Red] Add ShouldValueTriplets Test case
    
    [Green] ShouldValueTriplets
    
    [Green] Add ShouldBeWorthless Test case
    
    [Red] Add Dice_11111_ShouldBe1200 Test case
    
    [Green] Dice_51115511111_ShouldBe2700
    
    [Green] Add Dice_11131_ShouldBe1100 Test case
    
    [Green] Add ShouldValueMixedSets Test case
    
    [Refactor]

---
+++ GreedIsGood/Kata.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GreedIsGood
+{
+    public class Kata
+    {
+        private readonly Dictionary<string, int> _scoreTable = new Dictionary<string, int>
+            {
+                {"111", 1000},
+                {"666", 600},
+                {"555", 500},
+                {"444", 400},
+                {"333", 300},
+                {"222", 200},
+                {"1", 100},
+                {"5", 50}
+            };
+
+        public int Score(int[] throwDice)
+        {
+            var orderThrowDice = throwDice.OrderBy(x => x).Select(x => x).ToArray();
+            var stringDice = orderThrowDice.Aggregate("", (current, dice) => current + dice);
+            var score = 0;
+            for (var i = 0; i < stringDice.Length;)
+            {
+                var countOfDice = stringDice.Count(x => x.Equals(stringDice[i]));
+                var dice = "";
+                if (countOfDice / 3 > 0)
+                {
+                    dice = stringDice.Substring(0, 3);
+                    stringDice = stringDice.Remove(0, 3);
+                }
+                else
+                {
+                    dice = stringDice.Substring(0, 1);
+                    stringDice = stringDice.Remove(0, 1);
+                }
+                if (this._scoreTable.ContainsKey(dice))
+                {
+                    score += this._scoreTable[dice];
+                }
+            }
+            return score;
+        }
+    }
+}
---
+++ GreedIsGood/ScoreChecker.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GreedIsGood
+{
+    [TestClass]
+    public class ScoreChecker
+    {
+        [TestMethod]
+        public void ShouldValueMixedSets()
+        {
+            var target = new Kata();
+            var throwDice = new int[] { 2, 4, 4, 5, 4 };
+
+            var actual = target.Score(throwDice);
+
+            var expected = 450;
+            Assert.AreEqual(expected, actual, "Should be 450");
+        }
+
+        [TestMethod]
+        public void Dice_11131_ShouldBe1100()
+        {
+            var target = new Kata();
+            var throwDice = new int[] { 1, 1, 1, 3, 1 };
+
+            var actual = target.Score(throwDice);
+
+            var expected = 1100;
+            Assert.AreEqual(expected, actual, "Should be 1100");
+        }
+
+        [TestMethod]
+        public void Dice_51115511111_ShouldBe2700()
+        {
+            var target = new Kata();
+            var throwDice = new int[] { 5, 1, 1, 1, 5, 5, 1, 1, 1, 1, 1 };
+
+            var actual = target.Score(throwDice);
+
+            var expected = 2700;
+            Assert.AreEqual(expected, actual, "Should be 2700");
+        }
+
+        [TestMethod]
+        public void ShouldBeWorthless()
+        {
+            var target = new Kata();
+            var throwDice = new int[] { 2, 3, 4, 6, 2 };
+
+            var actual = target.Score(throwDice);
+
+            var expected = 0;
+            Assert.AreEqual(expected, actual, "Should be 0 :-(");
+        }
+
+        [TestMethod]
+        public void ShouldValueTriplets()
+        {
+            var target = new Kata();
+            var throwDice = new int[] { 4, 4, 4, 3, 3 };
+
+            var actual = target.Score(throwDice);
+
+            var expected = 400;
+            Assert.AreEqual(expected, actual, "Should be 400");
+        }
+    }
+}
