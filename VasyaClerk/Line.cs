df88504d5f3c74130f9343953f3bcfcc0acc9feb
Angus <ajdfajdfl2003@gmail.com>
2017-04-16 14:18:53 +0800

    Finish Vasya - Clerk
    Vasya - Clerk Test init
    
    [Red] Add peopleInLine_25_25_50_50_return_Yes Test case
    
    [Green] peopleInLine_25_25_50_50_return_Yes
    
    [Green] Add peopleInLine_25_100_return_No Test case
    
    [Red] Add peopleInLine_25_25_50_100_return_No Test case
    
    [Green] peopleInLine_25_25_50_100_return_No
    
    [Refactor]
    
    [Red] Add peoopleInLine_255025100252525100252550100252550100252525100_Return_Yes Test case
    
    [Greed] peoopleInLine_255025100252525100252550100252550100252525100_Return_Yes
    
    [Refactor]

---
+++ VasyaClerk/Line.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace VasyaClerk
+{
+    public class Line
+    {
+        public string Tickets(int[] peopleInLine)
+        {
+            var giveChanges = peopleInLine.Select(money => money - 25).ToList();
+            var inComeOrderByDescending = peopleInLine.OrderByDescending(x => x).ToList();
+            giveChanges.RemoveAll(x => x.Equals(0));
+            for (var i = 0; i < giveChanges.Count; i++)
+            {
+                var j = 0;
+                var round = 0;
+                do
+                {
+                    if (j == inComeOrderByDescending.Count)
+                    {
+                        j = 0;
+                        round++;
+                        if (round == 2)
+                        {
+                            break;
+                        }
+                    }
+                    if (inComeOrderByDescending[j] != 0 && giveChanges[i] % inComeOrderByDescending[j] == 0)
+                    {
+                        giveChanges[i] -= inComeOrderByDescending[j];
+                        inComeOrderByDescending[j] -= inComeOrderByDescending[j];
+                        j = 0;
+                    }
+                    j++;
+                } while (giveChanges[i] > 0);
+                inComeOrderByDescending.RemoveAll(x => x.Equals(0));
+            }
+            return giveChanges.Count(x => x > 0) > 0 ? "NO" : "YES";
+        }
+    }
+}
---
+++ VasyaClerk/LineTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VasyaClerk
+{
+    [TestClass]
+    public class LineTests
+    {
+        [TestMethod]
+        public void peoopleInLine_255025100252525100252550100252550100252525100_Return_Yes()
+        {
+            var target = new Line();
+            var peopleInLine = new int[] { 25, 50, 25, 100, 25, 25, 25, 100, 25, 25, 50, 100, 25, 25, 50, 100, 25, 25, 25, 100 };
+
+            var actual = target.Tickets(peopleInLine);
+            Assert.AreEqual("YES", actual);
+        }
+
+        [TestMethod]
+        public void peopleInLine_25_25_50_100_return_No()
+        {
+            var target = new Line();
+            var peopleInLine = new int[] { 25, 25, 50, 100 };
+
+            var actual = target.Tickets(peopleInLine);
+            Assert.AreEqual("YES", actual);
+        }
+
+        [TestMethod]
+        public void peopleInLine_25_100_return_No()
+        {
+            var target = new Line();
+            var peopleInLine = new int[] { 25, 100 };
+
+            var actual = target.Tickets(peopleInLine);
+            Assert.AreEqual("NO", actual);
+        }
+
+        [TestMethod]
+        public void peopleInLine_25_25_50_50_return_Yes()
+        {
+            var target = new Line();
+            var peopleInLine = new int[] { 25, 25, 50, 50 };
+
+            var actual = target.Tickets(peopleInLine);
+            Assert.AreEqual("YES", actual);
+        }
+    }
+}
