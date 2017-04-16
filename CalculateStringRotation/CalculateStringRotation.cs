2ad41b29c9e969b4799794ccbb957f9d4211f232
Angus <ajdfajdfl2003@gmail.com>
2017-04-16 19:34:46 +0800

    Finish Calculate String Rotation
    Calculate String Rotation Test init
    
    [Red] Add ShiftedDiff_eecoff_To_coffee_Return_4 Test case
    
    [Green] ShiftedDiff_eecoff_To_coffee_Return_4
    
    [Green] Add ShiftedDiff_Esham_To_Esham_Return_0 Test case
    
    [Green] Add ShiftedDiff_Moose_To_moose_Return_nil Test case
    
    [Refactor]
    
    [Green] Add ShiftedDiff_isnt_To_tisn_Return_2 Test case
    
    [Green] Add ShiftedDiff_empty_To_empty_Return_0 Test case
    
    [Green] Add ShiftedDiff_hoop_To_pooh_Return_nil Test case
    
    [Green] Add ShiftedDiff_twoEmpty_To_oneEmpty_Return_nil Test case

---
+++ CalculateStringRotation/CalculateStringRotation.cs
@@ -0,0 +1,20 @@
+﻿namespace CalculateStringRotation
+{
+    public class CalculateStringRotation
+    {
+        public int ShiftedDiff(string first, string second)
+        {
+            var count = 0;
+            while (count < first.Length)
+            {
+                if (first == second)
+                {
+                    return count;
+                }
+                first = first.Substring(first.Length - 1, 1) + first.Remove(first.Length - 1, 1);
+                count++;
+            }
+            return -1;
+        }
+    }
+}
---
+++ CalculateStringRotation/CalculateStringRotationTests.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculateStringRotation
+{
+    [TestClass]
+    public class CalculateStringRotationTests
+    {
+        [TestMethod]
+        public void ShiftedDiff_twoEmpty_To_oneEmpty_Return_nil()
+        {
+            var target = new CalculateStringRotation();
+            var first = "  ";
+            var second = " ";
+
+            var actual = target.ShiftedDiff(first, second);
+
+            Assert.AreEqual(-1, actual);
+        }
+
+        [TestMethod]
+        public void ShiftedDiff_hoop_To_pooh_Return_nil()
+        {
+            var target = new CalculateStringRotation();
+            var first = "";
+            var second = "";
+
+            var actual = target.ShiftedDiff(first, second);
+
+            Assert.AreEqual(-1, actual);
+        }
+
+        [TestMethod]
+        public void ShiftedDiff_empty_To_empty_Return_0()
+        {
+            var target = new CalculateStringRotation();
+            var first = " ";
+            var second = " ";
+
+            var actual = target.ShiftedDiff(first, second);
+
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void ShiftedDiff_isnt_To_tisn_Return_2()
+        {
+            var target = new CalculateStringRotation();
+            var first = "isn't";
+            var second = "'tisn";
+
+            var actual = target.ShiftedDiff(first, second);
+
+            Assert.AreEqual(2, actual);
+        }
+
+        [TestMethod]
+        public void ShiftedDiff_Moose_To_moose_Return_nil()
+        {
+            var target = new CalculateStringRotation();
+            var first = "Moose";
+            var second = "moose";
+
+            var actual = target.ShiftedDiff(first, second);
+
+            Assert.AreEqual(-1, actual);
+        }
+
+        [TestMethod]
+        public void ShiftedDiff_Esham_To_Esham_Return_0()
+        {
+            var target = new CalculateStringRotation();
+            var first = "Esham";
+            var second = "Esham";
+
+            var actual = target.ShiftedDiff(first, second);
+
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void ShiftedDiff_eecoff_To_coffee_Return_4()
+        {
+            var target = new CalculateStringRotation();
+            var first = "eecoff";
+            var second = "coffee";
+
+            var actual = target.ShiftedDiff(first, second);
+
+            Assert.AreEqual(4, actual);
+        }
+    }
+}
