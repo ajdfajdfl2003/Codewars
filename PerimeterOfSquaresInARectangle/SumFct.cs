10fdd694e787e836aa7f758de0ebba6fb237698a
Angus <ajdfajdfl2003@gmail.com>
2017-04-23 14:30:36 +0800

    Finish Perimeter of squares in a rectangle
    Perimeter of squares in a rectangle Test init
    
    [Red] Add PerimeterTest_Input_5_Result_80 Test case
    
    [Green] PerimeterTest_Input_5_Result_80
    
    [Green] Add PerimeterTest_Input_7_Result_216 Test case
    
    [Refactor]

---
+++ PerimeterOfSquaresInARectangle/SumFct.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace PerimeterOfSquaresInARectangle
+{
+    public static class SumFct
+    {
+        public static BigInteger perimeter(BigInteger n)
+        {
+            var fibSequence = new List<BigInteger> {1, 1};
+            n += 1;
+            BigInteger i = 2;
+            while (i < n)
+            {
+                var n1 = (int) i - 1;
+                var n2 = (int) i - 2;
+                fibSequence.Add(fibSequence[n1] + fibSequence[n2]);
+                i++;
+            }
+            BigInteger result = 0;
+            foreach (var fn in fibSequence)
+            {
+                result += fn;
+            }
+            return 4 * result;
+        }
+    }
+}
---
+++ PerimeterOfSquaresInARectangle/SumFctTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Numerics;
+
+namespace PerimeterOfSquaresInARectangle
+{
+    [TestClass]
+    public class SumFctTests
+    {
+        [TestMethod]
+        public void PerimeterTest_Input_7_Result_216()
+        {
+            Assert.AreEqual(new BigInteger(216), SumFct.perimeter(new BigInteger(7)));
+        }
+
+        [TestMethod]
+        public void PerimeterTest_Input_5_Result_80()
+        {
+            Assert.AreEqual(new BigInteger(80), SumFct.perimeter(new BigInteger(5)));
+        }
+    }
+}
