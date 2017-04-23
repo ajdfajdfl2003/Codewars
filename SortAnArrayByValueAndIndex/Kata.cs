df540b3185b3ef76fe6097e245ae29ead0d9c908
Angus <ajdfajdfl2003@gmail.com>
2017-04-23 14:16:02 +0800

    [Refactor] Refactor Sort an array by value and index to Best practice

--- SortAnArrayByValueAndIndex/Kata.cs
+++ SortAnArrayByValueAndIndex/Kata.cs
@@ -5,25 +5,10 @@ namespace SortAnArrayByValueAndIndex
 {
     public class Kata
     {
-        public Kata()
-        {
-        }
-
         public int[] SortByValueAndIndex(int[] array)
         {
-            var result = new List<Weights>();
-            for (var i = 0; i < array.Length; i++)
-            {
-                var weight = new Weights() {original = array[i], weightOfSum = array[i] * (i + 1)};
-                result.Add(weight);
-            }
-            return result.OrderBy(x => x.weightOfSum).Select(x => x.original).ToArray();
+            var i = 1;
+            return array.OrderBy(x => x * i++).ToArray();
         }
-    }
-
-    public class Weights
-    {
-        public int original { get; set; }
-        public int weightOfSum { get; set; }
     }
 }
