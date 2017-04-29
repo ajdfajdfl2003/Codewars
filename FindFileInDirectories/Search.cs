33fe009ad56324619d5e8b5a15eca6a48ce9e2e5
Angus <ajdfajdfl2003@gmail.com>
2017-04-29 21:17:49 +0800

    [Refactor] Refactor Find File in Directories to Best practices

--- FindFileInDirectories/Search.cs
+++ FindFileInDirectories/Search.cs
@@ -1,28 +1,21 @@
-﻿namespace FindFileInDirectories
+﻿using System.Linq;
+
+namespace FindFileInDirectories
 {
     public class Search
     {
         public string FindFileInFolder(Folder folder, string fileName)
         {
-            var result = $@"{folder.Name}\\";
-            foreach(var item in folder.Items)
+            if (folder.Items
+                .Select(x => x.Name)
+                .Contains(fileName))
             {
-                if(item is Folder)
-                {
-                    result += $@"{FindFileInFolder((Folder)item,fileName)}";
-                    if(result.Contains(fileName))
-                    {
-                        return result;
-                    }
-                    result = result.Replace($@"{item.Name}\\","");
-                }
-                if(item.Name.Equals(fileName))
-                {
-                    result += $@"{item.Name}";
-                    return result;
-                }
+                return folder.Name + $@"\\" + fileName;
             }
-            return result;
+            return folder.Name + $@"\\" +
+                   folder.Items.OfType<Folder>()
+                       .Select(x => FindFileInFolder(x, fileName))
+                       .FirstOrDefault(x => x.Contains(fileName));
         }
     }
 }
