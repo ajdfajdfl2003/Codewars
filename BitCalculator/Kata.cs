348756caa4bc40bd404d8cc53a3502f5f99b6168
Angus <ajdfajdfl2003@gmail.com>
2017-05-06 10:59:17 +0800

    Finish Bit calculator
    Bit calculator Test init
    
    [Red] Add CalculateTest_1_Add_1_equal_2 Test case
    
    [Green] CalculateTest_1_Add_1_equal_2
    
    [Red] Add CalculateTest_10_Add_10_equal_4 Test case
    
    [Green] CalculateTest_10_Add_10_equal_4
    
    [Refactor]
    
    [Green] Add CalculateTest_10_Add_0_equal_2 Test case
    
    [Green] Add CalculateTest_10_Add_1_equal_3 Test case

---
+++ BitCalculator/Kata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace BitCalculator
+{
+    public class Kata
+    {
+        public int Calculate(string num1, string num2)
+        {
+            var charsOfNum1 = num1.Reverse().ToArray();
+            var result = charsOfNum1.Select((t, i) => (t - '0') * (int) Math.Round(Math.Pow(2, i))).Sum();
+
+            var charsOfNum2 = num2.Reverse().ToArray();
+            result += charsOfNum2.Select((t, i) => (t - '0') * (int) Math.Round(Math.Pow(2, i))).Sum();
+
+            return result;
+        }
+    }
+}
---
+++ BitCalculator/SolutionTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BitCalculator
+{
+    [TestClass]
+    public class SolutionTest
+    {
+        [TestMethod]
+        public void CalculateTest_10_Add_1_equal_3()
+        {
+            var target = new Kata();
+
+            var actual = target.Calculate("10", "1");
+
+            var expected = 3;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculateTest_10_Add_0_equal_2()
+        {
+            var target = new Kata();
+
+            var actual = target.Calculate("10", "0");
+
+            var expected = 2;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculateTest_10_Add_10_equal_4()
+        {
+            var target = new Kata();
+
+            var actual = target.Calculate("10", "10");
+
+            var expected = 4;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CalculateTest_1_Add_1_equal_2()
+        {
+            var target = new Kata();
+
+            var actual = target.Calculate("1", "1");
+
+            var expected = 2;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
