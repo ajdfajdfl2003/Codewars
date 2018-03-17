5988eae0479a61929d0748039142a326376635f5
AngusWu <anguswu@simbo.com.tw>
2018-03-17 21:10:10 +0800

    Refactor: Change logic

--- Java/Dinglemouse/src/main/java/com/angus/codewars/Dinglemouse.java
+++ Java/Dinglemouse/src/main/java/com/angus/codewars/Dinglemouse.java
@@ -3,26 +3,16 @@ package com.angus.codewars;
 public class Dinglemouse {
     public static int[] humanYearsCatYearsDogYears(final int humanYears) {
         int cat = 0, dog = 0;
-        for (int i = 0; i < 3; i++) {
-            switch (i) {
-                case 0:
-                    if (humanYears - 1 >= 0) {
-                        cat = dog = 15;
-                    }
-                    break;
-                case 1:
-                    if (humanYears - 2 >= 0) {
-                        cat = dog += 9;
-                    }
-                    break;
-                case 2:
-                    int remain = humanYears - 2;
-                    if (remain >= 0) {
-                        cat += remain * 4;
-                        dog += remain * 5;
-                    }
-                    break;
-            }
+        switch (humanYears) {
+            default:
+                cat += (humanYears - 2) * 4;
+                dog += (humanYears - 2) * 5;
+            case 2:
+                cat += 9;
+                dog += 9;
+            case 1:
+                cat += 15;
+                dog += 15;
         }
         return new int[]{humanYears, cat, dog};
     }
