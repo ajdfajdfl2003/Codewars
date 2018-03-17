918cd1945674ad8442df021b2c92992ed1901b71
AngusWu <anguswu@simbo.com.tw>
2018-03-17 21:01:57 +0800

    fixup

--- Java/Dinglemouse/src/main/java/com/angus/codewars/Dinglemouse.java
+++ Java/Dinglemouse/src/main/java/com/angus/codewars/Dinglemouse.java
@@ -14,6 +14,7 @@ public class Dinglemouse {
                     if (humanYears - 2 >= 0) {
                         cat = dog += 9;
                     }
+                    break;
             }
         }
         return new int[]{humanYears, cat, dog};
