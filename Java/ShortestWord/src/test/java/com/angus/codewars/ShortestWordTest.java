b7819c867511c237310882d28367ded316323922
AngusWu <anguswu@simbo.com.tw>
2018-03-17 16:33:42 +0800

    Finish test case two from Codewars
    - Given "turns out random test cases are easier than writing out basic
    ones" should return 3

--- Java/ShortestWord/src/test/java/com/angus/codewars/ShortestWordTest.java
+++ Java/ShortestWord/src/test/java/com/angus/codewars/ShortestWordTest.java
@@ -34,4 +34,14 @@ public class ShortestWordTest {
 
         assertEquals(3, actual);
     }
+
+    @Test
+    public void GivenTestCase_Two_Should_Return3() {
+        Kata target = new Kata();
+
+        String givenWord = "turns out random test cases are easier than writing out basic ones";
+        int actual = target.findShort(givenWord);
+
+        assertEquals(3, actual);
+    }
 }
