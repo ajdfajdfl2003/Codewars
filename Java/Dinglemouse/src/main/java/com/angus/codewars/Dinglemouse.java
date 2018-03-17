06e997a1890283206b59f3ac337789b35eff5c4d
AngusWu <anguswu@simbo.com.tw>
2018-03-17 20:54:25 +0800

    Finish given human two-year-old should get 24 for both cat and dog

--- Java/Dinglemouse/src/main/java/com/angus/codewars/Dinglemouse.java
+++ Java/Dinglemouse/src/main/java/com/angus/codewars/Dinglemouse.java
@@ -2,8 +2,20 @@ package com.angus.codewars;
 
 public class Dinglemouse {
     public static int[] humanYearsCatYearsDogYears(final int humanYears) {
-        int cat = humanYears * 15;
-        int dog = humanYears * 15;
+        int cat = 0, dog = 0;
+        for (int i = 0; i < 3; i++) {
+            switch (i) {
+                case 0:
+                    if (humanYears - 1 >= 0) {
+                        cat = dog = 15;
+                    }
+                    break;
+                case 1:
+                    if (humanYears - 2 >= 0) {
+                        cat = dog += 9;
+                    }
+            }
+        }
         return new int[]{humanYears, cat, dog};
     }
 }
--- Java/Dinglemouse/src/test/java/com/angus/codewars/DinglemouseTest.java
+++ Java/Dinglemouse/src/test/java/com/angus/codewars/DinglemouseTest.java
@@ -6,6 +6,14 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class DinglemouseTest {
     @Test
+    public void testGiven_TwoYearOld_Should_Get_2_24_24() {
+        int[] actual = Dinglemouse.humanYearsCatYearsDogYears(2);
+
+        int[] expected = new int[]{2, 24, 24};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
     public void testGiven_OneYearOld_Should_Get_1_15_15() {
         int[] actual = Dinglemouse.humanYearsCatYearsDogYears(1);
 
