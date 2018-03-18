d276eae7809c73ceb253d8b956bec5b7c540c722
AngusWu <anguswu@simbo.com.tw>
2018-03-18 20:10:47 +0800

    Refactor: replace for-loop, use Stream instead

--- Java/SortTheOdd/src/main/java/com/angus/Kata.java
+++ Java/SortTheOdd/src/main/java/com/angus/Kata.java
@@ -1,6 +1,8 @@
 package com.angus;
 
 import java.util.Arrays;
+import java.util.PrimitiveIterator;
+import java.util.stream.IntStream;
 
 public class Kata {
     public static Kata newInstance() {
@@ -8,14 +10,10 @@ public class Kata {
     }
 
     public int[] sortArray(int[] array) {
-        int[] sortedOdd = Arrays.stream(array).filter(x -> x % 2 != 0).sorted().toArray();
-        int flag = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 != 0) {
-                array[i] = sortedOdd[flag];
-                flag++;
-            }
-        }
-        return array;
+        PrimitiveIterator.OfInt sortedOdd = IntStream.of(array).filter(e -> e % 2 == 1).sorted().iterator();
+
+        return IntStream.of(array)
+                .map(e -> e % 2 == 0 ? e : sortedOdd.nextInt())
+                .toArray();
     }
 }
