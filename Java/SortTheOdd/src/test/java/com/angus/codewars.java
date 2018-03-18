3faaacb49c3ae93bcca3034f7b3f657d7fb0145e
AngusWu <anguswu@simbo.com.tw>
2018-03-18 19:58:22 +0800

    Finish exampleTest2

--- Java/SortTheOdd/src/test/java/com/angus/codewars.java
+++ Java/SortTheOdd/src/test/java/com/angus/codewars.java
@@ -10,4 +10,10 @@ public class codewars {
         int[] actual = Kata.newInstance().sortArray(new int[]{5, 3, 2, 8, 1, 4});
         assertThat(actual).isEqualTo(new int[]{1, 3, 2, 8, 5, 4});
     }
+
+    @Test
+    public void exampleTest2() {
+        int[] actual = Kata.newInstance().sortArray(new int[]{5, 3, 1, 8, 0});
+        assertThat(actual).isEqualTo(new int[]{1, 3, 5, 8, 0});
+    }
 }
