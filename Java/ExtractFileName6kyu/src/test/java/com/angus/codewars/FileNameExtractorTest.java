209d9e3ad80e7651946b3c71334181462ea900df
AngusWu <anguswu@simbo.com.tw>
2018-03-25 13:34:43 +0800

    Pass test case "1_This_is_an_otherExample.mpg.OTHEREXTENSIONadasdassdassds34"

--- Java/ExtractFileName6kyu/src/test/java/com/angus/codewars/FileNameExtractorTest.java
+++ Java/ExtractFileName6kyu/src/test/java/com/angus/codewars/FileNameExtractorTest.java
@@ -6,6 +6,12 @@ import static org.junit.Assert.assertEquals;
 
 public class FileNameExtractorTest {
     @Test
+    public void testSample03() {
+        assertEquals("This_is_an_otherExample.mpg"
+                , FileNameExtractor.extractFileName("1_This_is_an_otherExample.mpg.OTHEREXTENSIONadasdassdassds34"));
+    }
+
+    @Test
     public void testSample02() {
         assertEquals(
                 "FILE_NAME.EXTENSION",
