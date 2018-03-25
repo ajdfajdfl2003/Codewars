38d26e310ad709a3f61d946e7444caeb677aaaa5
AngusWu <anguswu@simbo.com.tw>
2018-03-25 15:12:17 +0800

    Pass test case test1230

--- Java/ValidateCreditCardNumber6ku/src/main/java/com/angus/codewars/Validate.java
+++ Java/ValidateCreditCardNumber6ku/src/main/java/com/angus/codewars/Validate.java
@@ -6,8 +6,9 @@ import java.util.stream.Stream;
 public class Validate {
     public static boolean validate(String n) {
         int[] checkValid = Stream.of(n.split("")).mapToInt(v -> Integer.parseInt(v)).toArray();
+        int needToCheked = checkValid.length % 2;
         for (int i = 0; i < checkValid.length; i++) {
-            if (i % 2 == 1) {
+            if (i % 2 == needToCheked) {
                 int replacement = checkValid[i] * 2;
                 replacement = replacement > 9 ? replacement - 9 : replacement;
                 checkValid[i] = replacement;
--- Java/ValidateCreditCardNumber6ku/src/test/java/com/angus/codewars/CreditcardValidationTest.java
+++ Java/ValidateCreditCardNumber6ku/src/test/java/com/angus/codewars/CreditcardValidationTest.java
@@ -6,6 +6,11 @@ import static org.junit.Assert.assertEquals;
 
 public class CreditcardValidationTest {
     @Test
+    public void test1230() {
+        assertEquals(true, Validate.validate("1230"));
+    }
+
+    @Test
     public void test891() {
         assertEquals(false, Validate.validate("891"));
     }
