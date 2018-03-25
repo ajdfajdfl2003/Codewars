268eafe26b410de772c8b677907f0b38069e2f9b
AngusWu <anguswu@simbo.com.tw>
2018-03-25 14:08:16 +0800

    Refactor: remove unnecessary

--- Java/ExtractFileName6kyu/src/main/java/com/angus/codewars/FileNameExtractor.java
+++ Java/ExtractFileName6kyu/src/main/java/com/angus/codewars/FileNameExtractor.java
@@ -1,22 +1,17 @@
 package com.angus.codewars;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FileNameExtractor {
     public static String extractFileName(String dirtyFileName) {
         String regexPattern = "^(\\d*)[_]([^ \\f\\n\\r\\t\\v]*)[.]([^ \\f\\n\\r\\t\\v]*)[.]";
-        List<String> result = new ArrayList<String>();
         Pattern registrarPattern = Pattern.compile(regexPattern);
         Matcher matcher = registrarPattern.matcher(dirtyFileName);
 
-        while (matcher.find()) {
-            result.add(matcher.group(2));
-            result.add(matcher.group(3));
+        if (matcher.find()) {
+            return matcher.group(2) + "." + matcher.group(3);
         }
-
-        return result.get(0) + "." + result.get(1);
+        return "";
     }
 }
