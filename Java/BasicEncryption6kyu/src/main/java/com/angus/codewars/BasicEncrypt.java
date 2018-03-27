a44c9ee8b82d7af654a937be0d6237d75c59567c
AngusWu <anguswu@simbo.com.tw>
2018-03-27 22:48:04 +0800

    Modify logic: When ascii code over 255

--- Java/BasicEncryption6kyu/src/main/java/com/angus/codewars/BasicEncrypt.java
+++ Java/BasicEncryption6kyu/src/main/java/com/angus/codewars/BasicEncrypt.java
@@ -2,14 +2,13 @@ package com.angus.codewars;
 
 public class BasicEncrypt {
     public String encrypt(String text, int rule) {
-
         if (text.isEmpty()) {
             return "";
         }
 
         char[] strToChars = text.toCharArray();
         for (int i = 0; i < strToChars.length; i++) {
-            strToChars[i] += rule;
+            strToChars[i] = (char) ((strToChars[i] + rule) % 256);
         }
 
         return new String(strToChars);
