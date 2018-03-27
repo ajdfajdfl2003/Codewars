820feffb47baa9d6637b1e1474c0299581651f79
AngusWu <anguswu@simbo.com.tw>
2018-03-27 09:37:01 +0800

    Pass test case makeReadable(60)

--- Java/HumanReadableTime5kyu/src/main/java/com/angus/codewars/HumanReadableTime.java
+++ Java/HumanReadableTime5kyu/src/main/java/com/angus/codewars/HumanReadableTime.java
@@ -5,8 +5,8 @@ public class HumanReadableTime {
 
     public static String makeReadable(int seconds) {
         int hour = 0;
-        int minute = 0;
-        int second = seconds;
+        int minute = seconds / 59;
+        int second = (seconds - minute) % 59;
         return String.format(formatStr, hour) + ":" + String.format(formatStr, minute) + ":" + String.format(formatStr, second);
     }
 }
--- Java/HumanReadableTime5kyu/src/test/java/com/angus/codewars/HumanReadableTimeTest.java
+++ Java/HumanReadableTime5kyu/src/test/java/com/angus/codewars/HumanReadableTimeTest.java
@@ -6,6 +6,11 @@ import static org.junit.Assert.assertEquals;
 
 public class HumanReadableTimeTest {
     @Test
+    public void testCase03() {
+        assertEquals("makeReadable(60)", "00:01:00", HumanReadableTime.makeReadable(60));
+    }
+
+    @Test
     public void testCase02() {
         assertEquals("makeReadable(5)", "00:00:05", HumanReadableTime.makeReadable(5));
     }
