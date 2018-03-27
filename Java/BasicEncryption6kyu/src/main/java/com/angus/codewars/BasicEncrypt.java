8644a6e7113f949a93bf6dc52bde6360580f880f
AngusWu <anguswu@simbo.com.tw>
2018-03-27 22:36:08 +0800

    Pass test case encrypt(b)

--- Java/BasicEncryption6kyu/src/main/java/com/angus/codewars/BasicEncrypt.java
+++ Java/BasicEncryption6kyu/src/main/java/com/angus/codewars/BasicEncrypt.java
@@ -2,9 +2,16 @@ package com.angus.codewars;
 
 public class BasicEncrypt {
     public String encrypt(String text, int rule) {
+
         if (text.isEmpty()) {
             return "";
         }
-        return null;
+
+        char[] strToChars = text.toCharArray();
+        for (int i = 0; i < strToChars.length; i++) {
+            strToChars[i]++;
+        }
+
+        return new String(strToChars);
     }
 }
--- Java/BasicEncryption6kyu/src/test/java/com/angus/codewars/BasicEncryptTest.java
+++ Java/BasicEncryption6kyu/src/test/java/com/angus/codewars/BasicEncryptTest.java
@@ -1,13 +1,26 @@
 package com.angus.codewars;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class BasicEncryptTest {
+    private BasicEncrypt enc;
+
+    @Before
+    public void setUp() throws Exception {
+        this.enc = new BasicEncrypt();
+    }
+
+    @Test
+    public void testDecrypt02() {
+        assertEquals("text = 'a', rule = 1", "b",
+                enc.encrypt("a", 1));
+    }
+
     @Test
     public void testDecrypt01() throws Exception {
-        BasicEncrypt enc = new BasicEncrypt();
         assertEquals("text = '', rule = 1", "",
                 enc.encrypt("", 1));
     }
