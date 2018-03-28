ee5eedc7f43fb8bf265dce7857cac48ef3abf663
AngusWu <anguswu@simbo.com.tw>
2018-03-28 13:00:32 +0800

    Pass test case longToIp(0)

--- Java/Int32ToIPv45kyu/src/test/java/com/angus/codewars/longToIPTests.java
+++ Java/Int32ToIPv45kyu/src/test/java/com/angus/codewars/longToIPTests.java
@@ -6,6 +6,11 @@ import static org.junit.Assert.assertEquals;
 
 public class longToIPTests {
     @Test
+    public void sampleTest_0() {
+        assertEquals("0.0.0.0", Kata.longToIP(0));
+    }
+
+    @Test
     public void sampleTest_2154959208L() {
         assertEquals("128.32.10.1", Kata.longToIP(2149583361L));
     }
