b0cec94188077724eec54fa16fe7523eb9da0f25
AngusWu <anguswu@simbo.com.tw>
2018-03-28 19:13:59 +0800

    Pass test case test_47ohms

--- Java/ResistorColorCodesPartII5kyu/src/test/java/com/angus/codewars/EncodeResistorColorsTest.java
+++ Java/ResistorColorCodesPartII5kyu/src/test/java/com/angus/codewars/EncodeResistorColorsTest.java
@@ -6,6 +6,11 @@ import static org.junit.Assert.assertEquals;
 
 public class EncodeResistorColorsTest {
     @Test
+    public void test_47ohms() {
+        assertEquals("yellow violet black gold", EncodeResistorColors.encodeResistorColors("47 ohms"));
+    }
+
+    @Test
     public void test_10ohms() {
         assertEquals("brown black black gold", EncodeResistorColors.encodeResistorColors("10 ohms"));
     }
