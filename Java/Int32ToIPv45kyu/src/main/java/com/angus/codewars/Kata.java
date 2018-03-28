cddf77c064aefbf56223c7fcab520400011ef7b4
AngusWu <anguswu@simbo.com.tw>
2018-03-28 13:12:15 +0800

    Refactor: use Bitwise and Bit Shift Operators

--- Java/Int32ToIPv45kyu/src/main/java/com/angus/codewars/Kata.java
+++ Java/Int32ToIPv45kyu/src/main/java/com/angus/codewars/Kata.java
@@ -5,21 +5,10 @@ public class Kata {
     private static final int BIT_MAX = 31;
 
     public static String longToIP(long ip) {
-        StringBuilder binary = new StringBuilder();
-        long tmp = ip;
-        for (int i = BIT_MAX; i >= 0; i--) {
-            long powValue = Math.round(Math.pow(2, i));
-            if (Math.round(tmp / powValue) == 1) {
-                binary.append(1);
-                tmp = tmp - powValue;
-            } else {
-                binary.append(0);
-            }
-        }
-        String result = "";
-        for (int i = 0; i < 4; i++) {
-            result += String.valueOf(Integer.parseInt(binary.substring(i * 8, (i + 1) * 8), 2)) + (i < 3 ? "." : "");
-        }
-        return result.toString();
+        long ip4 = ip & 0xFF;
+        long ip3 = (ip >> 8) & 0xFF;
+        long ip2 = (ip >> 16) & 0xFF;
+        long ip1 = (ip >> 24) & 0xFF;
+        return String.format("%d.%d.%d.%d", ip1, ip2, ip3, ip4);
     }
 }
