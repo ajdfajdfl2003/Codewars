a9b1e9be6823fda1aa7fe97041c20ae947c6d3de
AngusWu <anguswu@simbo.com.tw>
2018-03-28 22:12:51 +0800

    Refactor logic

--- Java/ResistorColorCodesPartII5kyu/src/main/java/com/angus/codewars/EncodeResistorColors.java
+++ Java/ResistorColorCodesPartII5kyu/src/main/java/com/angus/codewars/EncodeResistorColors.java
@@ -1,45 +1,23 @@
 package com.angus.codewars;
 
-import java.util.HashMap;
-
 public class EncodeResistorColors {
-    public static String encodeResistorColors(String ohmsString) {
-        String[] splitOhmsStr = ohmsString.split(" ");
-        String ohmsValue = splitOhmsStr[0];
+    private static String[] CODES = new String[]{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "gray", "white"};
 
-        ohmsValue = calculateOhms(ohmsValue);
+    public static String encodeResistorColors(String ohmsString) {
+        String splitOhmsStr = ohmsString.split(" ")[0];
+        String ohmsValue = "" + (int) (Double.parseDouble(splitOhmsStr.replaceAll("[^\\.0-9]", "")) * calcFactor(splitOhmsStr));
 
         String result = "";
         String[] bands = ohmsValue.split("");
         for (int i = 0; i < 2; i++) {
-            result += getResistorColor().get(bands[i]) + " ";
+            result += CODES[Integer.parseInt(bands[i])] + " ";
         }
 
         int multiplier = ohmsValue.length() - 2;
-        return result + getResistorColor().get(String.valueOf(multiplier)) + " gold";
+        return result + CODES[multiplier] + " gold";
     }
 
-    private static String calculateOhms(String ohmsValue) {
-        if (ohmsValue.contains("k")) {
-            ohmsValue = String.valueOf((int) (Double.parseDouble(ohmsValue.substring(0, ohmsValue.indexOf("k"))) * 1000));
-        } else if (ohmsValue.contains("M")) {
-            ohmsValue = String.valueOf((int) (Double.parseDouble(ohmsValue.substring(0, ohmsValue.indexOf("M"))) * 1000000));
-        }
-        return ohmsValue;
-    }
-
-    private static HashMap<String, String> getResistorColor() {
-        HashMap<String, String> resistorColor = new HashMap<String, String>();
-        resistorColor.put("0", "black");
-        resistorColor.put("1", "brown");
-        resistorColor.put("2", "red");
-        resistorColor.put("3", "orange");
-        resistorColor.put("4", "yellow");
-        resistorColor.put("5", "green");
-        resistorColor.put("6", "blue");
-        resistorColor.put("7", "violet");
-        resistorColor.put("8", "gray");
-        resistorColor.put("9", "white");
-        return resistorColor;
+    private static int calcFactor(String ohmsValue) {
+        return ohmsValue.contains("k") ? 1000 : ohmsValue.contains("M") ? 1000000 : 1;
     }
 }
