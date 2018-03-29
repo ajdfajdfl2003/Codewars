c15a719747ba9d981b79a773de5e894126e0fb84
AngusWu <anguswu@simbo.com.tw>
2018-03-29 23:18:48 +0800

    Refactor:
    - remove if-else, use while() instead
    - make loss twice a month ratio to const.

--- Java/KataOfJava/src/main/java/com/angus/codewars/BuyCar.java
+++ Java/KataOfJava/src/main/java/com/angus/codewars/BuyCar.java
@@ -1,28 +1,26 @@
 package com.angus.codewars;
 
 public class BuyCar {
+
+    public static final double TWICE_MONTH_LOSS_RATIO = 0.005;
+
     public static int[] nbMonths(int startPriceOld, int startPriceNew, int savingperMonth, double percentLossByMonth) {
         int month = 0;
         int resetOfMoney;
         int totalAmount = 0;
 
-        if (startPriceOld > startPriceNew) {
-            resetOfMoney = startPriceOld - startPriceNew;
-        } else {
-            double ratio = 1 - (percentLossByMonth / 100);
-            do {
-                month++;
-                totalAmount += savingperMonth;
+        double ratio = 1 - (percentLossByMonth / 100);
+        while (startPriceOld + totalAmount < startPriceNew) {
+            month++;
+            totalAmount += savingperMonth;
 
-                double lossTwiceMonth = month % 2 == 0 ? 0.005 : 0;
-                ratio -= lossTwiceMonth;
-                startPriceOld *= ratio;
-                startPriceNew *= ratio;
-
-                resetOfMoney = startPriceOld - startPriceNew + totalAmount;
-            } while (resetOfMoney <= 0);
+            double lossTwiceMonth = month % 2 == 0 ? TWICE_MONTH_LOSS_RATIO : 0;
+            ratio -= lossTwiceMonth;
+            startPriceOld *= ratio;
+            startPriceNew *= ratio;
         }
 
+        resetOfMoney = startPriceOld + totalAmount - startPriceNew;
         return new int[]{month, resetOfMoney};
     }
 }
