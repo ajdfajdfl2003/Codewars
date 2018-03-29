60c2d7c180b62b57e18c34188f22d8080d88e3ee
AngusWu <anguswu@simbo.com.tw>
2018-03-29 23:11:36 +0800

    Pass  test case BuyCar.nbMonths(2000, 8000, 1000, 1.5)

--- Java/KataOfJava/src/main/java/com/angus/codewars/BuyCar.java
+++ Java/KataOfJava/src/main/java/com/angus/codewars/BuyCar.java
@@ -3,10 +3,26 @@ package com.angus.codewars;
 public class BuyCar {
     public static int[] nbMonths(int startPriceOld, int startPriceNew, int savingperMonth, double percentLossByMonth) {
         int month = 0;
-        int resetOfMoney = 0;
+        int resetOfMoney;
+        int totalAmount = 0;
+
         if (startPriceOld > startPriceNew) {
             resetOfMoney = startPriceOld - startPriceNew;
+        } else {
+            double ratio = 1 - (percentLossByMonth / 100);
+            do {
+                month++;
+                totalAmount += savingperMonth;
+
+                double lossTwiceMonth = month % 2 == 0 ? 0.005 : 0;
+                ratio -= lossTwiceMonth;
+                startPriceOld *= ratio;
+                startPriceNew *= ratio;
+
+                resetOfMoney = startPriceOld - startPriceNew + totalAmount;
+            } while (resetOfMoney <= 0);
         }
+
         return new int[]{month, resetOfMoney};
     }
 }
--- Java/KataOfJava/src/test/java/com/angus/codewars/BuyCarTest.java
+++ Java/KataOfJava/src/test/java/com/angus/codewars/BuyCarTest.java
@@ -6,9 +6,15 @@ import static org.junit.Assert.assertArrayEquals;
 
 public class BuyCarTest {
     @Test
+    public void test1() {
+        int[] r = new int[]{6, 766};
+        assertArrayEquals(r, BuyCar.nbMonths(2000, 8000, 1000, 1.5));
+    }
+
+    @Test
     public void test2() {
         //old car price larger than new car
-        int[] r = new int[] { 0, 4000 };
+        int[] r = new int[]{0, 4000};
         assertArrayEquals(r, BuyCar.nbMonths(12000, 8000, 1000, 1.5));
     }
 }
