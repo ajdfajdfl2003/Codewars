641457e4b81c4d32f1e90ac3489445dea57c8e91
AngusWu <anguswu@simbo.com.tw>
2018-03-29 19:07:14 +0800

    Move StopgninnipSMysdroWTest in

--- Java/KataOfJava/src/main/java/com/angus/codewars/StopgninnipSMysdroW.java
+++ Java/KataOfJava/src/main/java/com/angus/codewars/StopgninnipSMysdroW.java
@@ -1,8 +1,10 @@
+package com.angus.codewars;
+
 import java.util.Arrays;
 
 import static java.util.stream.Collectors.joining;
 
-public class SpinWords {
+public class StopgninnipSMysdroW {
     public String spinWords(String sentence) {
         return Arrays.stream(sentence.split(" ")).map(s -> s.length() >= 5 ? new StringBuilder(s).reverse()
                 .toString() : s).collect(joining(" "));
