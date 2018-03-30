ae490fd2e02d8a4af60ea8ac8c1a74a85619038c
AngusWu <anguswu@simbo.com.tw>
2018-03-30 17:09:29 +0800

    Pass test case GrowthOfAPopulation.nbYear(1500000, 2.5, 10000, 2000000)

--- Java/KataOfJava/src/test/java/com/angus/codewars/GrowthOfAPopulationTests.java
+++ Java/KataOfJava/src/test/java/com/angus/codewars/GrowthOfAPopulationTests.java
@@ -6,6 +6,11 @@ import static org.junit.Assert.assertEquals;
 
 public class GrowthOfAPopulationTests {
     @Test
+    public void test_pZero_1500000_p_2000000() {
+        assertEquals(10, GrowthOfAPopulation.nbYear(1500000, 2.5, 10000, 2000000));
+    }
+
+    @Test
     public void test_pZero_1500_p_5000() {
         assertEquals(15, GrowthOfAPopulation.nbYear(1500, 5, 100, 5000));
     }
