9a3c6b6d6b0512aaf3ada4571d762698c625ff79
AngusWu <anguswu@simbo.com.tw>
2018-03-31 23:43:56 +0800

    Refactor: extract repeat statement to method

--- Java/KataOfJava/src/main/java/com/angus/codewars/Accumul.java
+++ Java/KataOfJava/src/main/java/com/angus/codewars/Accumul.java
@@ -6,12 +6,16 @@ public class Accumul {
         String[] splitOfStr = s.split("");
         for (int i = 0; i < s.length(); i++) {
             result.append(splitOfStr[i].toUpperCase());
-            result.append(new String(new char[i]).replace("\0",
-                    splitOfStr[i].toLowerCase()));
+            result.append(getRepeatString(splitOfStr[i], i));
             if (i < s.length() - 1) {
                 result.append("-");
             }
         }
         return result.toString();
+    }
+
+    private static String getRepeatString(String waitToRepeat, int repeatTimes) {
+        return new String(new char[repeatTimes])
+                .replace("\0", waitToRepeat.toLowerCase());
     }
 }
