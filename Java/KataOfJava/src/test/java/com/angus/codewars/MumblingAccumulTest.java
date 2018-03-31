5fa85880de90233b104583e5c06059df144c4c3b
AngusWu <anguswu@simbo.com.tw>
2018-03-31 23:40:30 +0800

    Pass test case tesAccumu_NyffsGeyylB

--- Java/KataOfJava/src/test/java/com/angus/codewars/MumblingAccumulTest.java
+++ Java/KataOfJava/src/test/java/com/angus/codewars/MumblingAccumulTest.java
@@ -6,6 +6,11 @@ import static org.junit.Assert.assertEquals;
 
 public class MumblingAccumulTest {
     @Test
+    public void tesAccumu_NyffsGeyylB() {
+        assertEquals("N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb", Accumul.accum("NyffsGeyylB"));
+    }
+
+    @Test
     public void testAccumu_ZpglnRxqenU() {
         assertEquals("Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu",
                 Accumul.accum("ZpglnRxqenU"));
