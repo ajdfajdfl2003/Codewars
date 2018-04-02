d96037c9e5e8984eb8c17552ac6c85bc78df43b9
AngusWu <anguswu@simbo.com.tw>
2018-04-02 22:26:55 +0800

    Pass test case testExampleArray

--- Java/KataOfJava/src/main/java/com/angus/codewars/Max.java
+++ Java/KataOfJava/src/main/java/com/angus/codewars/Max.java
@@ -7,6 +7,20 @@ public class Max {
         if (Arrays.asList(arr).isEmpty()) {
             return 0;
         }
-        return 0;
+
+        int maxOfSum = 0, sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+
+            if (sum > 0) {
+                sum += arr[i];
+            } else {
+                sum = arr[i];
+            }
+
+            if (sum > maxOfSum) {
+                maxOfSum = sum;
+            }
+        }
+        return maxOfSum;
     }
 }
--- Java/KataOfJava/src/test/java/MaxSequenceTest.java
+++ Java/KataOfJava/src/test/java/MaxSequenceTest.java
@@ -1,11 +1,18 @@
 import com.angus.codewars.Max;
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class MaxSequenceTest {
     @Test
+    public void testExampleArray() throws Exception {
+        assertEquals("Example array should have a max of 6",
+                6, Max.sequence(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+
+    @Test
     public void testEmptyArray() throws Exception {
-        Assert.assertEquals("Empty arrays should have a max of 0",
+        assertEquals("Empty arrays should have a max of 0",
                 0, Max.sequence(new int[]{}));
     }
 }
