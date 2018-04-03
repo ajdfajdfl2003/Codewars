50b9a151a487ecdc9bbb8805a8e8d9483057eb34
AngusWu <anguswu@simbo.com.tw>
2018-04-03 19:20:04 +0800

    Pass test case PerfectGame

--- Java/KataOfJava/src/main/java/com/angus/codewars/TenPinBowling.java
+++ Java/KataOfJava/src/main/java/com/angus/codewars/TenPinBowling.java
@@ -1,13 +1,29 @@
 package com.angus.codewars;
 
 import java.util.Arrays;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class TenPinBowling {
     public static int bowling_score(String frames) {
         int lastScore = 0;
+        Queue<Integer> waitForCalculate = new LinkedBlockingQueue<>();
+
         String[] eachFrame = frames.split(" ");
         for (String score : eachFrame) {
-            lastScore += Arrays.stream(score.split("")).mapToInt(Integer::parseInt).sum();
+            if (score.contains("X")) {
+                waitForCalculate.add(10);
+                System.out.println(score);
+                if (waitForCalculate.size() == 3) {
+                    lastScore += waitForCalculate.stream().mapToInt(i -> i).sum();
+                    waitForCalculate.remove();
+                }
+                if (score.length() == 3) {
+                    lastScore += 60;
+                }
+            } else {
+                lastScore += Arrays.stream(score.split("")).mapToInt(Integer::parseInt).sum();
+            }
         }
         return lastScore;
     }
--- Java/KataOfJava/src/test/java/com/angus/codewars/TenPinBowlingTest.java
+++ Java/KataOfJava/src/test/java/com/angus/codewars/TenPinBowlingTest.java
@@ -6,6 +6,12 @@ import static org.junit.Assert.assertEquals;
 
 public class TenPinBowlingTest {
     @Test
+    public void PerfectGame() {
+        assertEquals(300,
+                TenPinBowling.bowling_score("X X X X X X X X X XXX"));
+    }
+
+    @Test
     public void BasicTests() {
         assertEquals(20,
                 TenPinBowling.bowling_score("11 11 11 11 11 11 11 11 11 11"));
