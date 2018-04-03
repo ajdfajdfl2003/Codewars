f71dbec167014c68f0294df1a8c5ae6c26e0e09a
AngusWu <anguswu@simbo.com.tw>
2018-04-04 00:20:27 +0800

    Add EncodeResistorColors comment

--- Java/KataOfJava/src/main/java/com/angus/codewars/EncodeResistorColors.java
+++ Java/KataOfJava/src/main/java/com/angus/codewars/EncodeResistorColors.java
@@ -1,10 +1,18 @@
 package com.angus.codewars;
 
+/*
+ 題目：https://www.codewars.com/kata/5855777bb45c01bada0002ac
+ */
 public class EncodeResistorColors {
     private static String[] CODES = new String[]{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "gray", "white"};
 
     public static String encodeResistorColors(String ohmsString) {
+        // 只取 ohms 前面的數字
         String splitOhmsStr = ohmsString.split(" ")[0];
+
+        // 先把後面有不是數字的取代掉
+        // 然後去乘上 相對應的數字，
+        // k -> 1000, M -> 1000000
         String ohmsValue = "" + (int) (Double.parseDouble(splitOhmsStr.replaceAll("[^\\.0-9]", "")) * calcFactor(splitOhmsStr));
 
         String result = "";
