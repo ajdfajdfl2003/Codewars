d2a062c30dbcd2cd9fe2e6159de0141a16b2f2e9
AngusWu <anguswu@simbo.com.tw>
2018-04-04 00:21:48 +0800

    Add FileNameExtractor comment

--- Java/KataOfJava/src/main/java/com/angus/codewars/FileNameExtractor.java
+++ Java/KataOfJava/src/main/java/com/angus/codewars/FileNameExtractor.java
@@ -3,6 +3,9 @@ package com.angus.codewars;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/*
+ 題目：https://www.codewars.com/kata/597770e98b4b340e5b000071
+ */
 public class FileNameExtractor {
     public static String extractFileName(String dirtyFileName) {
         String regexPattern = "^(\\d*)[_]([^ \\f\\n\\r\\t\\v]*)[.]([^ \\f\\n\\r\\t\\v]*)[.]";
