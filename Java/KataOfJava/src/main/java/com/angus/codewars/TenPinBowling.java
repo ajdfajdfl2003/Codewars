dfba6d28f8d62e39d88c6f90c18b608014e09dfa
AngusWu <anguswu@simbo.com.tw>
2018-04-03 21:52:08 +0800

    Refactor logic

--- Java/KataOfJava/src/main/java/com/angus/codewars/TenPinBowling.java
+++ Java/KataOfJava/src/main/java/com/angus/codewars/TenPinBowling.java
@@ -1,30 +1,32 @@
 package com.angus.codewars;
 
-import java.util.Arrays;
-import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
-
 public class TenPinBowling {
     public static int bowling_score(String frames) {
-        int lastScore = 0;
-        Queue<Integer> waitForCalculate = new LinkedBlockingQueue<>();
+        int lastScore = 0, gameRoll = 0, index = 0;
+        String[] eachFrame = frames.replace(" ", "").split("(?<!^)");
 
-        String[] eachFrame = frames.split(" ");
-        for (String score : eachFrame) {
-            if (score.contains("X")) {
-                waitForCalculate.add(10);
-                System.out.println(score);
-                if (waitForCalculate.size() == 3) {
-                    lastScore += waitForCalculate.stream().mapToInt(i -> i).sum();
-                    waitForCalculate.remove();
-                }
-                if (score.length() == 3) {
-                    lastScore += 60;
-                }
+        do {
+            if (eachFrame[gameRoll].equalsIgnoreCase("x")) {
+                lastScore += 10
+                        + checkScore(eachFrame[gameRoll + 1])
+                        + checkScore(eachFrame[gameRoll + 2]);
+                gameRoll++;
             } else {
-                lastScore += Arrays.stream(score.split("")).mapToInt(Integer::parseInt).sum();
+                lastScore += Integer.parseInt(eachFrame[gameRoll]);
+                if (index % 2 == 1) {
+                    gameRoll++;
+                }
             }
-        }
+            index++;
+        } while (gameRoll < 10);
+
         return lastScore;
+    }
+
+    private static int checkScore(String score) {
+        if (score.equalsIgnoreCase("x")) {
+            return 10;
+        }
+        return 0;
     }
 }
