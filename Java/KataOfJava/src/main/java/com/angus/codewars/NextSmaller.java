f50d0088757c28c89d69c9fc45051de9e485a729
AngusWu <anguswu@simbo.com.tw>
2018-04-05 21:34:32 +0800

    Pass test case testCase_907_790

--- Java/KataOfJava/src/main/java/com/angus/codewars/NextSmaller.java
+++ Java/KataOfJava/src/main/java/com/angus/codewars/NextSmaller.java
@@ -1,7 +1,9 @@
 package com.angus.codewars;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.LongStream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -20,16 +22,37 @@ public class NextSmaller {
             }
         }
 
+        String firstDigit = allDigits.substring(0, 1);
         /* Sorted without first digit */
         List<String> sortedPartialDigits = Arrays.stream(allDigits.substring(1, allDigits.length())
                 .split("")).sorted().collect(toList());
-        sortedPartialDigits.add(0, allDigits.substring(0, 1));
+        sortedPartialDigits.add(0, firstDigit);
 
         if (compareWithOrigin(n, Long.parseLong(String.join("", sortedPartialDigits)))) {
             return Long.parseLong(String.join("", sortedPartialDigits));
         }
 
-        return Long.parseLong(String.join("", sortedPartialDigits));
+        /* Find most closet and smaller than first digit */
+        List<Long> closetDigit = LongStream.range(0, Long.parseLong(firstDigit))
+                .filter(digit -> sortedPartialDigits.contains(String.valueOf(digit)))
+                .boxed().sorted(Comparator.reverseOrder()).collect(toList());
+        swapDigit(sortedPartialDigits, firstDigit, closetDigit.get(0));
+        List<String> result = sortedPartialDigits.subList(1, sortedPartialDigits.size())
+                .stream().sorted(Comparator.reverseOrder()).collect(toList());
+        result.add(0, sortedPartialDigits.get(0));
+
+        if (compareWithOrigin(n, Long.parseLong(String.join("", result)))) {
+            return Long.parseLong(String.join("", result));
+        }
+
+        return -1;
+    }
+
+    private static void swapDigit(List<String> sortedPartialDigits, String element, Long closetDigit) {
+        int elementIndex = sortedPartialDigits.indexOf(element);
+        int closetDigitIndex = sortedPartialDigits.indexOf(String.valueOf(closetDigit));
+        sortedPartialDigits.set(elementIndex, String.valueOf(closetDigit));
+        sortedPartialDigits.set(closetDigitIndex, element);
     }
 
     private static boolean compareWithOrigin(long n, long comparison) {
--- Java/KataOfJava/src/test/java/com/angus/codewars/NextSmallerTest.java
+++ Java/KataOfJava/src/test/java/com/angus/codewars/NextSmallerTest.java
@@ -6,6 +6,12 @@ import static org.junit.Assert.assertEquals;
 
 public class NextSmallerTest {
     @Test
+    public void testCase_907_790() {
+        assertEquals("NextSmaller.nextSmaller(907)"
+                , 790, NextSmaller.nextSmaller(907));
+    }
+
+    @Test
     public void testCase_531_513() {
         assertEquals(513, NextSmaller.nextSmaller(531));
     }
