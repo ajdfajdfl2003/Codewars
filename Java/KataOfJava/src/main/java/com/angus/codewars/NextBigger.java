bb1ddb960a65cbdeb3ebe6a8a189aa5d65a54947
AngusWu <anguswu@simbo.com.tw>
2018-04-07 13:25:13 +0800

    Pass test case test2024249490

--- Java/KataOfJava/src/main/java/com/angus/codewars/NextBigger.java
+++ Java/KataOfJava/src/main/java/com/angus/codewars/NextBigger.java
@@ -1,5 +1,7 @@
 package com.angus.codewars;
 
+import java.util.Arrays;
+
 public class NextBigger {
     public static long nextBiggerNumber(long n) {
         String[] splitOfDigits = String.valueOf(n).split("");
@@ -9,14 +11,14 @@ public class NextBigger {
         for (int i = 0; i < splitOfDigits.length - 1; i++) {
             if (getLong(splitOfDigits[i]) < getLong(splitOfDigits[i + 1])) {
                 flag = true;
-                index = i + 1;
+                index = i;
             }
         }
 
         if (!flag) return -1;
 
-        for (int i = index; i >= 0; i--) {
-            if (getLong(splitOfDigits[index]) > getLong(splitOfDigits[i])) {
+        for (int i = index + 1; i < splitOfDigits.length; i++) {
+            if (getLong(splitOfDigits[i]) > getLong(splitOfDigits[index])) {
                 String tmp = splitOfDigits[i];
                 splitOfDigits[i] = splitOfDigits[index];
                 splitOfDigits[index] = tmp;
@@ -24,7 +26,17 @@ public class NextBigger {
             }
         }
 
-        return getLong(String.join("", splitOfDigits));
+        String[] partialResult = Arrays.stream(convertToString(splitOfDigits)
+                .substring(index + 1, splitOfDigits.length)
+                .split("")).sorted().toArray(String[]::new);
+        String result = convertToString(splitOfDigits).substring(0, index + 1)
+                + convertToString(partialResult);
+
+        return getLong(result);
+    }
+
+    private static String convertToString(String[] splitOfDigits) {
+        return String.join("", splitOfDigits);
     }
 
     private static long getLong(String digit) {
--- Java/KataOfJava/src/test/java/com/angus/codewars/NextBiggerTest.java
+++ Java/KataOfJava/src/test/java/com/angus/codewars/NextBiggerTest.java
@@ -6,6 +6,18 @@ import static org.junit.Assert.assertEquals;
 
 public class NextBiggerTest {
     @Test
+    public void test2024249490() {
+        assertEquals(2024249904, NextBigger.nextBiggerNumber(2024249490));
+    }
+
+    @Test
+    public void basicNegativeTest() {
+        assertEquals(-1, NextBigger.nextBiggerNumber(9));
+        assertEquals(-1, NextBigger.nextBiggerNumber(111));
+        assertEquals(-1, NextBigger.nextBiggerNumber(531));
+    }
+
+    @Test
     public void basicTests() {
         assertEquals(21, NextBigger.nextBiggerNumber(12));
         assertEquals(531, NextBigger.nextBiggerNumber(513));
