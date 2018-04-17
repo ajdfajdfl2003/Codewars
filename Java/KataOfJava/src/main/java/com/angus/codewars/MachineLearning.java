99ad69542cfbbb66368578109249b94ee4d8e232
AngusWu <anguswu@simbo.com.tw>
2018-04-17 22:27:28 +0800

    Pass first test case

---
+++ Java/KataOfJava/src/main/java/com/angus/codewars/MachineLearning.java
@@ -0,0 +1,26 @@
+package com.angus.codewars;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+/*
+ 題目：https://www.codewars.com/kata/the-learning-game-machine-learning-number-1
+ */
+public class MachineLearning {
+    static class Actions {
+        static final List<Function<Integer, Integer>> FUNCTIONS = Arrays.asList(x -> x + 1, x -> 0, x -> x % 2, x -> x / 2, x -> x * 100);
+    }
+
+    public int command(int cmd, int num) {
+        int result = 0;
+        if (cmd == 0) {
+            return 0;
+        }
+        return result;
+    }
+
+    public void response(boolean result) {
+
+    }
+}
