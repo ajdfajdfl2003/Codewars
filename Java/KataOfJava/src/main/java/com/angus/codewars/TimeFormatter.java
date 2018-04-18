223a5a14786d39784740ee6fffbd6d3db6f11be0
AngusWu <anguswu@simbo.com.tw>
2018-04-19 01:14:44 +0800

    Pass test case 1 minute and 2 seconds

--- Java/KataOfJava/src/main/java/com/angus/codewars/TimeFormatter.java
+++ Java/KataOfJava/src/main/java/com/angus/codewars/TimeFormatter.java
@@ -2,6 +2,20 @@ package com.angus.codewars;
 
 public class TimeFormatter {
     public static String formatDuration(int seconds) {
-        return String.format("%d second", seconds);
+        int transToMinutes = seconds / 60;
+        int transToSeconds = seconds - transToMinutes * 60;
+
+        String formatMinutes = transToMinutes + (transToMinutes > 1 ? " minutes" : " minute");
+        String formatSeconds = transToSeconds + (transToSeconds > 1 ? " seconds" : " second");
+
+        StringBuilder result = new StringBuilder();
+        if (transToMinutes != 0) {
+            result.append(formatMinutes + " and ");
+        }
+        if (transToSeconds != 0) {
+            result.append(formatSeconds);
+        }
+
+        return result.toString();
     }
 }
--- Java/KataOfJava/src/test/java/com/angus/codewars/HumanReadableDurationFormatTest.java
+++ Java/KataOfJava/src/test/java/com/angus/codewars/HumanReadableDurationFormatTest.java
@@ -9,4 +9,9 @@ public class HumanReadableDurationFormatTest {
     public void testFormatDuration_1second() {
         assertEquals("1 second", TimeFormatter.formatDuration(1));
     }
+
+    @Test
+    public void testFormatDuration_1minutes_2seconds() {
+        assertEquals("1 minute and 2 seconds", TimeFormatter.formatDuration(62));
+    }
 }
