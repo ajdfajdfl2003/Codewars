9222198766e4dbb5fd275b673cc2c5703bbfc858
AngusWu <anguswu@simbo.com.tw>
2018-07-20 00:00:41 +0800

    Metro: the most short solution

--- Java/KataOfJava/src/main/java/com/angus/codewars/Metro.java
+++ Java/KataOfJava/src/main/java/com/angus/codewars/Metro.java
@@ -1,21 +1,12 @@
 package com.angus.codewars;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /*
  題目：https://www.codewars.com/kata/number-of-people-in-the-bus
  */
 public class Metro {
     public int countPassengers(ArrayList<int[]> stops) {
-        int result = 0;
-
-        for (int[] stop : stops) {
-            result += Arrays.stream(stop).reduce((left, right) ->
-                    left - right
-            ).getAsInt();
-        }
-
-        return result;
+        return stops.stream().mapToInt(stop -> stop[0] - stop[1]).sum();
     }
 }
