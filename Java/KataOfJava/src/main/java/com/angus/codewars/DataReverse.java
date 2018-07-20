3fa6db22a41bccf9bc70afdd1dbbdece5730a629
AngusWu <anguswu@simbo.com.tw>
2018-07-21 00:51:46 +0800

    the most clever solution

--- Java/KataOfJava/src/main/java/com/angus/codewars/DataReverse.java
+++ Java/KataOfJava/src/main/java/com/angus/codewars/DataReverse.java
@@ -1,16 +1,9 @@
 package com.angus.codewars;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class DataReverse {
     public static int[] DataReverse(int[] data) {
-        List<Integer> result = new LinkedList<>();
-        for (int i = data.length / 8 - 1; i >= 0; i--) {
-            result.addAll(Arrays.stream(data).skip(i * 8L).limit(8L).boxed().collect(Collectors.toList()));
-        }
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return java.util.stream.IntStream.range(0, data.length)
+                .map(i -> data[data.length - 8 - (i / 8 * 8) + (i % 8)])
+                .toArray();
     }
 }
